201e834ff8a71c4efb3dfd1d2a1c88c5ada3cd4d
bquoc0411 <hoangquocbinh0411@gmail.com>
2025-02-06 17:26:51 +0700

    initial commit

---
+++ src/main/java/com/example/datnsd26/DatnSd26Application.java
@@ -0,0 +1,13 @@
+package com.example.datnsd26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnSd26Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnSd26Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datnsd26/DatnSd26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datnsd26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnSd26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
