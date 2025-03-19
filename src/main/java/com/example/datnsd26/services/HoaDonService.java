c84b1bcd4a5bd16bd07f6ea302a75af3e8b277bd
JavaTech04 <nonghoangvu2004@gmail.com>
2025-03-19 22:31:32 +0700

    #17 - Display invoice detail data

--- src/main/java/com/example/datnsd26/services/HoaDonService.java
+++ src/main/java/com/example/datnsd26/services/HoaDonService.java
@@ -1,8 +1,11 @@
 package com.example.datnsd26.services;
 
 import com.example.datnsd26.controller.request.InvoiceParamRequest;
+import com.example.datnsd26.controller.response.InvoiceInformation;
 import com.example.datnsd26.controller.response.InvoicePageResponse;
 
 public interface HoaDonService {
     InvoicePageResponse getInvoices(InvoiceParamRequest request);
+
+    InvoiceInformation getInvoice(Integer id);
 }
